fd9d37678ff822f40f5557b8fb4f4313a8af3caf
Harshh2426 <100610013+Harshh2426@users.noreply.github.com>
2022-12-11 11:36:24 +0530

    Variables in js

---
+++ 01_Variables.js
@@ -0,0 +1,22 @@
+//  4 ways to declare js variable
+
+//  1) using var
+
+var a = 10;
+console.log(a);
+
+// 2) using let
+
+let b = 10;
+console.log(b);
+
+// 3) using const
+
+const c = 10;
+console.log(c);
+
+// 4) direct
+// -> Here js compiler automatically create var d=10 if variable is directly declared
+
+d = 10;
+console.log(d);
